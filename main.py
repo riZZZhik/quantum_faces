096448c00ae07b602a5e60f7ab1696d3e332d76f
riZZZhik <oldreddemon@gmail.com>
2021-01-25 23:04:22 +0300

    [ADD] Initialize and compile Keras model in Quantum init function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from quantum import Quantum
 
 if __name__ == "__main__":
-    q = Quantum("dataset/CelebA", "dataset/labels.txt", batch_size=16, nqubits=4,
+    q = Quantum((128, 128, 3), "dataset/CelebA", "dataset/labels.txt", batch_size=16, nqubits=4,
                 dataset_delta=1, label_max_filter=100)
     q.train(100)
--- quantum/quantum.py
+++ quantum/quantum.py
@@ -1,24 +1,18 @@
-import copy
 import sys
-import time
 
 import pennylane as qml
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from facenet_pytorch import InceptionResnetV1
+from keras import Model
+from keras.applications import Xception
+from keras.layers import Dense, Flatten
 from loguru import logger
-from torch.optim import lr_scheduler
 
 from .quantumnet import Quantumnet
-from .utils import get_celeba_generator
 from .utils_quantum import H_layer, RY_layer, entangling_layer
 
 
 class Quantum:  # TODO: Comments
-    def __init__(self, images_dir, labels_path, batch_size, dataset_delta=40, label_max_filter=None,
-                 nqubits=32, q_depth=4, q_delta=0.01, max_layers=15, step=0.001, gamma_lr_scheduler=.1,
-                 log_file="logs.log"):
+    def __init__(self, image_shape, images_dir, labels_path, batch_size, dataset_delta=40, label_max_filter=None,
+                 nqubits=32, q_depth=4, q_delta=0.01, max_layers=15, log_file="logs.log"):
         # Init logger
         if log_file:
             logger.add(log_file)
@@ -26,20 +20,21 @@ class Quantum:  # TODO: Comments
 
         # Init variables
         self.batch_size = batch_size
+        self.image_shape = image_shape
 
         self.nqubits = nqubits
         self.q_depth = q_depth
         self.q_delta = q_delta
         self.max_layers = max_layers
 
+        freeze_imagenet = False
+
         # Init dataset
         logger.info("Initializing dataset")
-        self.dataset_generators, self.dataset_sizes, self.num_classes = \
-            get_celeba_generator(batch_size, images_dir, labels_path, dataset_delta, label_max_filter)
+        # TODO: Dataset Generator
 
         # Init torch device
-        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-        logger.info(f'Running PyTorch on device: {self.device}')
+        # TODO: Init tensorflow device
 
         # Init quantum
         try:
@@ -50,22 +45,25 @@ class Quantum:  # TODO: Comments
                             'sudo sh -c "/usr/bin/echo 1 > /proc/sys/vm/overcommit_memory"')
             sys.exit(-1)
 
-        self.q_net = self._get_q_net_function()
-        self.quantumnet = Quantumnet(self.device, self.q_net, self.nqubits, self.q_delta,
-                                     self.max_layers, list(range(self.num_classes)))
+        # Initialize XCeption + QuantumNet model
+        base_model = Xception(input_shape=self.image_shape, weights='imagenet', include_top=False)
 
-        # Init facenet
-        self.model = InceptionResnetV1(classify=True, num_classes=nqubits)
-        self.model.logits = self.quantumnet
+        x = base_model.output
+        x = Flatten()(x)
+        x = Dense(nqubits)(x)
+        x = Quantumnet(self._get_q_net_function(), self.nqubits, self.q_delta, self.max_layers)(x)
+        predictions = Dense(self.num_classes)(x)
 
-        # for param in self.model.parameters():  # FIXME: Error with frozen facenet
-        #     param.requires_grad = False
-        self.model = self.model.to(self.device)
+        self.model = Model(base_model.input, predictions)
+        print(self.model.summary())
 
-        # Init training variables
-        self.criterion = nn.CrossEntropyLoss()
-        self.optimizer = optim.Adam(self.model.logits.parameters(), lr=step)
-        self.exp_lr_scheduler = lr_scheduler.StepLR(self.optimizer, step_size=3, gamma=gamma_lr_scheduler)
+        if freeze_imagenet:
+            for layer in base_model.layers:
+                layer.trainable = False
+
+        # Compile model
+        self.model.compile(optimizer="nadam", loss='categorical_crossentropy', metrics=['accuracy'])
+        # self.exp_lr_scheduler = lr_scheduler.StepLR(self.optimizer, step_size=3, gamma=gamma_lr_scheduler)  # TODO: do we need this in keras?
 
     def _get_q_net_function(self):
         @qml.qnode(self.backend, interface='torch')
@@ -90,73 +88,4 @@ class Quantum:  # TODO: Comments
         return q_net_circuit
 
     def train(self, num_epochs):
-        since = time.time()
-        best_model_wts = copy.deepcopy(self.model.state_dict())
-        best_acc = 0.0
-        best_loss = 10000.0  # Large arbitrary number
-        best_acc_train = 0.0
-        best_loss_train = 10000.0  # Large arbitrary number
-        logger.info('Training started:')
-        for epoch in range(num_epochs):
-            # Each epoch has a training and validation phase
-            for phase in ['train', 'val']:
-                if phase == 'train':
-                    # Set model to training mode
-                    self.exp_lr_scheduler.step()
-                    self.model.train()
-                else:
-                    # Set model to evaluate mode
-                    self.model.eval()
-
-                    # Iteration loop
-                running_loss = 0.0
-                running_corrects = 0
-                n_batches = self.dataset_sizes[phase] // self.batch_size
-                it = 0
-                for x, y in self.dataset_generators[phase]():
-                    since_batch = time.time()
-                    batch_size_ = len(x)
-                    x = x.to(self.device)
-                    y = y.to(self.device)
-                    self.optimizer.zero_grad()
-
-                    # Track/compute gradient and make an optimization step only when training
-                    with torch.set_grad_enabled(phase == 'train'):
-                        outputs = self.model(x)
-                        _, preds = torch.max(outputs, 1)
-                        loss = self.criterion(outputs, y)
-                        if phase == 'train':
-                            loss.backward()
-                            self.optimizer.step()
-
-                    # Print iteration results
-                    running_loss += loss.item() * batch_size_
-                    batch_corrects = torch.sum(preds == y.data).item()
-                    running_corrects += batch_corrects
-                    logger.info('Phase: {} Epoch: {}/{} Iter: {}/{} Batch time: {:.4f}'.format(
-                        phase, epoch + 1, num_epochs, it + 1, n_batches + 1, time.time() - since_batch))
-                    it += 1
-
-                # Print epoch results
-                epoch_loss = running_loss / self.dataset_sizes[phase]
-                epoch_acc = running_corrects / self.dataset_sizes[phase]
-                logger.info('Phase: {} Epoch: {}/{} Loss: {:.4f} Acc: {:.4f}'.format(
-                    'train' if phase == 'train' else 'val  ', epoch + 1, num_epochs, epoch_loss, epoch_acc))
-
-                # Check if this is the best model wrt previous epochs
-                if phase == 'val' and epoch_acc > best_acc:
-                    best_acc = epoch_acc
-                    best_model_wts = copy.deepcopy(self.model.state_dict())
-                if phase == 'val' and epoch_loss < best_loss:
-                    best_loss = epoch_loss
-                if phase == 'train' and epoch_acc > best_acc_train:
-                    best_acc_train = epoch_acc
-                if phase == 'train' and epoch_loss < best_loss_train:
-                    best_loss_train = epoch_loss
-
-        # Print final results
-        self.model.load_state_dict(best_model_wts)
-        time_elapsed = time.time() - since
-        logger.info('Training completed in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
-        logger.info('Best test loss: {:.4f} | Best test accuracy: {:.4f}'.format(best_loss, best_acc))
-        return self.model
+        pass  # TODO: Model training
